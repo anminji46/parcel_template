34e21a3558f653fc0ca31623d1065d76b2622f45
anminji46 <dksdksalswl@naver.com>
2024-11-25 16:24:09 +0900

    parcel bundler project

---
+++ .postcssrc.js
@@ -0,0 +1,14 @@
+// node.js -> common.js
+module.exports = {
+  plugins : [
+    require('autoprefixer')
+  ]
+};
+
+// ES
+// import autoprefixer from 'autoprefixer';
+// export {
+//   plugins : [
+//     autoprefixer
+//   ]
+// }
